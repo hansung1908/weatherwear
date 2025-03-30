ccf600af587a44b5013ccd8c9764c20cb3959534
hansung1908 <hansung1908@naver.com>
2025-03-30 22:57:21 +0900

    프로젝트 생성

---
+++ src/test/java/com/hansung/weatherwear/WeatherwearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansung.weatherwear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherwearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
